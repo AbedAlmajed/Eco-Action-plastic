fdcb7c34359c6b9de884ca5ba50776ff573673b4
AbedAlmajed <abedalmajedalajarmah@gmail.com>
2024-10-15 07:50:22 +0300

    added some thing

--- User/next.config.mjs
+++ User/next.config.mjs
@@ -5,26 +5,11 @@
 
 
 
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     images: {
-//       domains: ['firebasestorage.googleapis.com'],    },
-//   };
-  
-//   export default nextConfig;
-  /////
-
-  /** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['firebasestorage.googleapis.com'],
-  },
-  experimental: {
-    turbotrace: {
-      logLevel: 'error',
-      logDetail: true,
-    },
-  },
-};
-
-export default nextConfig;
+    images: {
+      domains: ['firebasestorage.googleapis.com'],    },
+  };
+  
+  export default nextConfig;
+  
